6a386a2f020f9eae17f66e810f85a81a6a67b346
danwanh <dananh890@gmail.com>
2025-11-01 10:56:29 +0700

    chore: setup fullstack project structure (react + vite + express + mongodb)

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("DB error:", err));
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
