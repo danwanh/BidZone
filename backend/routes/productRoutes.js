9bdcb51b23e4898658438a017548f956f11b6eac
sodene <giaobao2kk5@gmail.com>
2025-11-11 15:57:12 +0700

    Added crud for product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,15 @@
 import express from  "express";
+import { addProduct, getAllProducts, getProductById, getProductBySellerId, deleteProductById, changeProductById } from "../controllers/productController.js";
+import upload from "../config/multer.js";
 const router = express.Router();
 
-import {} from "../controllers/productController.js";
+const MAXIMUM_PICTURE_SENT = 25;
+
+router.post("/", addProduct);
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.get("/:id/seller", getProductBySellerId);
+router.delete("/:id", deleteProductById);
+router.patch("/:id", changeProductById);
 
 export default router;
