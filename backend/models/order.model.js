e6217ce6f44d57ecb30907b38283f18ad2582bb1
trong2108 <138958891+trong2108@users.noreply.github.com>
2025-11-15 04:11:46 +0700

    Add files via upload

---
+++ backend/models/order.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const OrderSchema = mongoose.Schema(
+    {
+        product_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true
+        },
+
+        seller_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+        buyer_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+        address: {
+            type: String,
+            required: true
+        },
+
+        invoice_info: {
+            type: String
+        },
+
+        status: {
+            type: String,
+            enum: ["unpaid", "paid", "pending", "delivered", "cancel"],
+            default: "pending"
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default mongoose.model("Order", OrderSchema);
---
+++ backend/models/upgrade_req.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const UpgradeRequestSchema = mongoose.Schema(
+    {
+        user_id: {
+            type: mongoose.Schema.Types,
+            ref: "User",
+            required: true
+        },
+
+        admin_id: {
+            type: mongoose.Schema.Types,
+            ref: "User"
+        },
+
+        status: {
+            type: String,
+            enum: ["pending", "rejected", "accepted"],
+            default: "pending"
+        },
+
+        note: {
+            type: String
+        }
+    },
+    {
+        timestamp: true
+    }
+);
+
+export default mongoose.model("UpgradeRequest", UpgradeRequestSchema);
