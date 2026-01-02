ff04308d26a783a5c52f47b760089756c2d087d5
Chis <chitran06052005@gmail.com>
2026-01-02 21:11:59 +0700

    minor fix for detail info display

--- frontend/src/components/home/Navbar.jsx
+++ frontend/src/components/home/Navbar.jsx
@@ -111,6 +111,7 @@ const Navbar = () => {
             {isOpen && (
               <div
                 className="absolute top-full left-0 mt-2 flex gap-0 z-50"
+                onMouseEnter={() => setIsOpen(true)}
                 onMouseLeave={() => {
                   setIsOpen(false);
                   setHoveredCategory(null);
