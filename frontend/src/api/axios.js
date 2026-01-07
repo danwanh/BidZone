67447110425f1bd89bc17411e653dc5eb15ed21a
danwanh <dananh890@gmail.com>
2026-01-07 14:22:53 +0700

    fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL =  process.env.VITE_API_URL;
+export const BASE_URL =  process.env.VITE_API_URL;
 const api = axios.create({
   baseURL: BASE_URL, //backend
   withCredentials: true, // required to send/receive httpOnly cookies
